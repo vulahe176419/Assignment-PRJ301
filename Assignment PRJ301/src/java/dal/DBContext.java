9e0521ef9b54b2c9792e2c92d3f2980f71c35b9d
vulahe176419 <162027794+vulahe176419@users.noreply.github.com>
2024-03-05 19:27:32 +0700

    connect db

---
+++ Assignment PRJ301/src/java/dal/DBContext.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author leanh
+ */
+public abstract class DBContext<T> {
+    
+    protected Connection connection;
+    
+    public DBContext() {
+        try {
+            String user = "sa";
+            String pass = "123456";
+            String url = "jdbc:sqlserver://DESKTOP-4F5U4RO\\SQLEXPRESS:1433;databaseName=AssignmentSP2024;encrypt=true;trustServerCertificate=true";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public abstract ArrayList<T> list();
+    public abstract void insert(T entity);
+    public abstract void update(T entity);
+    public abstract void delete(T entity);
+    public abstract T get(int id);
+}
